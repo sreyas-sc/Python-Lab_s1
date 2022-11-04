774c04966217a278c2aa465033df17d9da61e619
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2022-11-04 10:44:29 +0530

    Update Leap_year.py

--- Leap_year.py
+++ Leap_year.py
@@ -1,9 +1,13 @@
-from datetime import date
-_fn_year=int(input("Enter the final year: "));
-_current_date=date.today();
-_current_year=_current_date.year;
-_yr=[];
-for i in(range(_current_year,_fn_year+1)):
-    if i%4==0 or i%100==0 or i%400==0:
-        _yr.append(i);
-print("The List of all leap year upto "+str(_fn_year)+" is : \n",_yr);
+n=int(input("enter the start year "))
+m=int(input("enter the end year "))
+l=[]
+
+if m>n:
+    for i in range(n,m+1):
+        if(i%4==0):
+            l.append(i)
+else:
+    print("check for your input ")
+
+if len(l)!=0:
+    print(l)
