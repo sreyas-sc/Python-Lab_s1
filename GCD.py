1d5c7e9f9fd759e8f2a8a91baf3dda35ec4f829c
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2022-11-05 11:07:08 +0530

    Add files via upload

---
+++ GCD.py
@@ -0,0 +1,6 @@
+import math
+First=int(input("\n Enter the 1st number\n"))
+Second=int(input("\n Enter 2nd number\n"))
+Gcd=math.gcd(First,Second)
+print("Greatest common divisible of",First,Second,"is:\n")
+print(Gcd)
