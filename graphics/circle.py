1c02464377cb46ad21f77bf108bea4daa55ebd1b
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2022-12-09 09:58:24 +0530

    Add files via upload

---
+++ graphics/circle.py
@@ -0,0 +1,5 @@
+from math import pi
+
+
+def area(radius: float):
+    return pi * radius * radius
---
+++ graphics/graphics_3d/cuboid.py
@@ -0,0 +1,3 @@
+def area(width: float, height: float, length: float):
+    
+    return 2*length*width + 2*length*height + 2*height*width
